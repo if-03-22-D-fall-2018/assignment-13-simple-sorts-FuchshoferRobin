5747b650b925e5ae8924fca6105b12a95b4c08ac
RobinFuchshofer <robinfuchshofer@gmail.com>
2019-01-31 13:31:05 +0100

    4 tests failed

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -12,14 +12,22 @@
 */
 #include "sorting_algorithms.h"
 
+#include "time.h"
+#include "unistd.h"
+#include <math.h>
+
 void init_random(int small_array[],int SMALL)
 {
-
+  srandom(time(NULL));
+  	for (unsigned long i = 0; i < SMALL; i++)
+  		small_array[i] = random();
 }
+
 void bubble_sort(int small_array[],int SMALL)
 {
 
 }
+
 void insertion_sort(int small_array[],int SMALL)
 {
 
